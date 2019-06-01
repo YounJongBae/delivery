f929cfcb998f59ee7eeadf28f69fbb10e73b2015
YounJongBae <yh3715@gmail.com>
2019-06-01 22:32:56 +0900

    git commit

---
+++ o2o/settings/local.py
@@ -0,0 +1,9 @@
+DEBUG = True
+ALLOWED_HOSTS = []
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
